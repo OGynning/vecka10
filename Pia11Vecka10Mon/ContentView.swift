10522abe613965e6e7db93a781bf4d49e8ae3f99
OGynning <113348004+OGynning@users.noreply.github.com>
2022-11-14 09:14:06 +0100

    Initial Commit

---
+++ Pia11Vecka10Mon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pia11Vecka10Mon
+//
+//  Created by oscar gynning on 2022-11-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pia11Vecka10Mon/Pia11Vecka10MonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pia11Vecka10MonApp.swift
+//  Pia11Vecka10Mon
+//
+//  Created by oscar gynning on 2022-11-14.
+//
+
+import SwiftUI
+
+@main
+struct Pia11Vecka10MonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
